71086660922eb908081d2ce942db4e276323762d
bobdavisdev <c4reddy@gmail.com>
2015-05-07 12:21:31 +0800

    init-db scipt modified for build error correction

--- init-db.sh
+++ init-db.sh
@@ -4,7 +4,7 @@ if [ "$INITDB" == true ]; then
 	service mysqld start & sleep 10
 	mysql -u root -e "CREATE DATABASE testdb;"
 	mysql -u root -e "set password for 'root'@'%' = PASSWORD('password');"
-	mysql -u root -e "GRANT ALL PRIVILEGES ON $MYSQL_DB.* TO 'root'@'%' IDENTIFIED BY 'password';"
+	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password';"
 else
 	echo "DB started"
 fi
